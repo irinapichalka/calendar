bb182e0ebc13cbe59ad2695e61119e2729d4a231
irinapichalka <irinarescan@gmail.com>
2022-10-20 16:47:57 +0300

    finished

--- scripts/calendar/calendar.js
+++ scripts/calendar/calendar.js
@@ -1,14 +1,18 @@
- import { getItem } from '../common/storage.js';
- import { generateWeekRange } from '../common/time.utils.js';
- import { renderEvents } from '../events/events.js';
- import { createNumbersArray } from '../common/createNumbersArray.js';
+import { getItem } from "../common/storage.js";
+import { generateWeekRange } from "../common/time.utils.js";
+import { renderEvents } from "../events/events.js";
+import { createNumbersArray } from "../common/createNumbersArray.js";
 
 const generateDay = () => {
   // функция должна сгенерировать и вернуть разметку дня в виде строки
   // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
-  const arrayTime = createNumbersArray(1,24);
-  const result = arrayTime.map((el,index) => `<div class="calendar__time-slot" data-time="${index}"></div>`)
-  .reduce((acc,el) => acc+el, ``);
+  const arrayTime = createNumbersArray(1, 24);
+  const result = arrayTime
+    .map(
+      (el, index) =>
+        `<div class="calendar__time-slot" data-time="${index}"></div>`
+    )
+    .reduce((acc, el) => acc + el, ``);
   return result;
 };
 
@@ -18,6 +22,7 @@ export const renderWeek = () => {
   // массив дней, которые нужно отобразить, считаем ф-цией generateWeekRange на основе displayedWeekStart из storage
   // каждый день должен содержать в дата атрибуте порядковый номер дня в месяце
   // после того, как отрисовали всю сетку для отображаемой недели, нужно отобразить события этой недели с помощью renderEvents
+
   const arrayDays = generateWeekRange(getItem("displayedWeekStart"));
   const htmlForWeeks = arrayDays.reduce((acc, el) => {
     return (
@@ -25,5 +30,5 @@ export const renderWeek = () => {
       `<div class="calendar__day" data-day="${el.getDate()}">${generateDay()}</div>`
     );
   }, ``);
-  document.querySelector('.calendar__week').innerHTML = htmlForWeeks;
+  document.querySelector(".calendar__week").innerHTML = htmlForWeeks;
 };
